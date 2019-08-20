99dc47956f0da5881d1e26154880e5e8687c07e3
lbeckman314 <lbeckman314@gmail.com>
2019-08-19 19:27:21 -0700

    Add resvg library for svg to png transformations.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ extern crate regex;
 use inflector::cases::kebabcase::to_kebab_case;
 use pulldown_cmark::{Event, Options, Parser, Tag};
 use regex::Regex;
+use resvg::prelude::*;
 use std::default::Default;
+use std::ffi::OsStr;
 use std::fs;
-use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::iter::repeat;
+use std::path::Path;
 use std::string::String;
 use walkdir::WalkDir;
 
@@ -18,11 +19,12 @@ enum EventType {
     Emphasis,
     Header,
     Strong,
+    Table,
     TableHead,
     Text,
 }
 
-struct CurrentType {
+pub struct CurrentType {
     event_type: EventType,
 }
 
@@ -98,7 +100,9 @@ pub fn markdown_to_latex(markdown: String) -> String {
             }
 
             Event::End(Tag::Paragraph) => {
-                output.push_str(r"\\");
+                // ~ adds a space to prevent
+                // "There's no line here to end" error on empty lines.
+                output.push_str(r"~\\");
                 output.push_str("\n");
             }
 
@@ -147,6 +151,7 @@ pub fn markdown_to_latex(markdown: String) -> String {
             }
 
             Event::Start(Tag::Table(_)) => {
+                current.event_type = EventType::Table;
                 output.push_str("\n");
                 output.push_str("\n");
                 output.push_str(r"\begingroup");
@@ -174,7 +179,9 @@ pub fn markdown_to_latex(markdown: String) -> String {
 
                 output.push_str(r"\hline");
                 output.push_str("\n");
-                current.event_type = EventType::Text;
+
+                // we presume that a table follows every table head.
+                current.event_type = EventType::Table;
             }
 
             Event::End(Tag::Table(_)) => {
@@ -195,6 +202,7 @@ pub fn markdown_to_latex(markdown: String) -> String {
                 }
                 output = output.replace("!!!", &cols);
                 cells = 0;
+                current.event_type = EventType::Text;
             }
 
             Event::Start(Tag::TableCell) => match current.event_type {
@@ -224,10 +232,24 @@ pub fn markdown_to_latex(markdown: String) -> String {
             }
 
             Event::Start(Tag::Image(_, path, title)) => {
+                let mut path_str = String::new();
+                path_str = path.clone().into_string();
+
+                // if image path ends with ".svg", run it through
+                // svg2png to convert to png file.
+                if get_extension(&path).unwrap() == "svg" {
+                    let img = svg2png(path.clone().into_string()).unwrap();
+
+                    let mut filename_png = String::from(path.clone().into_string());
+                    filename_png = filename_png.replace(".svg", ".png");
+                    img.save(std::path::Path::new(&filename_png));
+                    path_str = filename_png.clone();
+                }
+
                 output.push_str("\\begin{figure}\n");
                 output.push_str("\\centering\n");
                 output.push_str("\\includegraphics[width=\\textwidth]{");;
-                output.push_str(&format!("../../src/{path}", path = path));
+                output.push_str(&format!("../../src/{path}", path = path_str));
                 output.push_str("}\n");
                 output.push_str("\\caption{");
                 output.push_str(&*title);
@@ -256,47 +278,33 @@ pub fn markdown_to_latex(markdown: String) -> String {
 
             Event::Code(t) => {
                 output.push_str("\\lstinline|");
-                output.push_str(&*t.replace("…", "..."));
+                match current.event_type {
+                    EventType::Header => {
+                        output.push_str(
+                            &*t.replace("#", r"\#")
+                                .replace("…", "...")
+                                .replace("З", "3")
+                        )
+                    },
+                    _ => {
+                        output.push_str(
+                            &*t.replace("…", "...")
+                                .replace("З", "3")
+                                .replace("�", r"\�")
+                        )
+                    },
+                }
                 output.push_str("|");
             }
 
             Event::InlineHtml(t) => {
-                let mut latex = t.into_string();
-
-                // remove all "class=foo" and "id=bar".
-                let re = Regex::new(r#"\s(class|id)="[a-zA-Z0-9-_]*"#).unwrap();
-                latex = re.replace(&latex, "").to_string();
-                latex = latex.replace("/>", "");
-
-                if latex.contains("<code") {
-                    latex = latex
-                        .replace("<code>", r"\lstinline+")
-                        .replace("</code>", r"+");
-                    output.push_str(&latex);
-                } else if latex.contains("<span") {
-                    latex = latex.replace("<span", "").replace(r"</span>", "");
-                    output.push_str(&latex);
-                } else if latex.contains("<img") {
-                    //let src = Regex::new(r#"src="(.*?)"#).unwrap();
-                    let src = Regex::new(r#"src="([a-zA-Z/_.]*)"#).unwrap();
-                    let caps = src.captures(&latex).unwrap();
-                    let path = caps.get(1).unwrap().as_str();
-                    println!("path: {}", path);
-
-                    output.push_str("\\includegraphics[width=0.2\\textwidth]{");;
-                    output.push_str(&format!("../../src/{path}", path = path));
-                    output.push_str("}\n");
-                }
+                // convert common html patterns to latex
+                output.push_str(&html2tex(t.into_string(), &current));
             }
 
             Event::Html(t) => {
-                let latex = t
-                    .replace("</code>", r"\end{lstlisting}")
-                    .replace("<code class=\"language-", r"\begin{lstlisting}[language=")
-                    .replace("<pre>", "")
-                    .replace("</pre>", "")
-                    .replace("\">", "]");
-                output.push_str(&latex);
+                // convert common html patterns to latex
+                output.push_str(&html2tex(t.into_string(), &current));
             }
 
             Event::Text(t) => {
@@ -307,7 +315,8 @@ pub fn markdown_to_latex(markdown: String) -> String {
                     | EventType::Text
                     | EventType::Header => {
                         output.push_str(
-                            &*t.replace("&", r"\&")
+                            &*t.replace(r"\", r"\\")
+                                .replace("&", r"\&")
                                 .replace(r"\s", r"\textbackslash{}s")
                                 .replace(r"\w", r"\textbackslash{}w")
                                 .replace("_", r"\_")
@@ -319,9 +328,7 @@ pub fn markdown_to_latex(markdown: String) -> String {
                         );
                         header_value = t.into_string();
                     }
-                    _ => {
-                        output.push_str(&*t);
-                    }
+                    _ => output.push_str(&*t),
                 }
             }
 
@@ -341,16 +348,61 @@ pub fn markdown_to_latex(markdown: String) -> String {
     output
 }
 
-pub fn markdown_to_pdf(markdown: String) -> Result<Vec<u8>, tectonic::Error> {
-    tectonic::latex_to_pdf(markdown_to_latex(markdown))
+/// Simple HTML parser.
+/// Eventually I hope to use a mature HTML to LaTeX parser.
+/// Something like https://github.com/Adonai/html2md/
+pub fn html2tex(html: String, current: &CurrentType) -> String {
+    let mut latex = html;
+    let mut output = String::new();
+
+    // remove all "class=foo" and "id=bar".
+    let re = Regex::new(r#"\s(class|id)="[a-zA-Z0-9-_]*">"#).unwrap();
+    latex = re.replace(&latex, "").to_string();
+    if latex.contains("<img") {
+        //let src = Regex::new(r#"src="(.*?)"#).unwrap();
+        let src = Regex::new(r#"src="([a-zA-Z0-9-/_.]*)"#).unwrap();
+        let caps = src.captures(&latex).unwrap();
+        let path_raw = caps.get(1).unwrap().as_str();
+        let mut path = format!("../../src/{path}", path = path_raw);
+
+        // if path ends with ".svg", run it through
+        // svg2png to convert to png file.
+        if get_extension(&path).unwrap() == "svg" {
+            let img = svg2png(path.to_string()).unwrap();
+            path = path.replace(".svg", ".png");
+            println!("path!: {}", &path);
+            img.save(std::path::Path::new(&path));
+        }
+
+        match current.event_type {
+            EventType::Table => output.push_str("\\includegraphics[width=0.2\\textwidth]{"),
+            _ => output.push_str("\\includegraphics[width=\\textwidth]{"),
+        }
+
+        output.push_str(&path);
+        output.push_str("}\n");
+    } else {
+        latex = latex
+            .replace("/>", "")
+            .replace("<code>", r"\lstinline+")
+            .replace("</code>", r"+")
+            .replace("<span", "")
+            .replace(r"</span>", "");
+
+        // remove all HTML comments.
+        let re = Regex::new(r"<!--.*-->").unwrap();
+        output.push_str(&re.replace(&latex, ""));
+    }
+
+    output
 }
 
 /// Get the title of a Markdown file.
 ///
 /// Reads the first line of a Markdown file, strips any hashes and
 /// leading/trailing whitespace, and returns the title.
-/// https://codereview.stackexchange.com/questions/135013/rust-function-to-read-the-first-line-of-a-file-strip-leading-hashes-and-whitesp
-fn title_string<R>(mut rdr: R) -> String
+/// Source: https://codereview.stackexchange.com/questions/135013/rust-function-to-read-the-first-line-of-a-file-strip-leading-hashes-and-whitesp
+pub fn title_string<R>(mut rdr: R) -> String
 where
     R: BufRead,
 {
@@ -367,4 +419,23 @@ where
 
     // Trim the leading hashes and any whitespace
     first_line[last_hash..].trim().into()
+}
+
+/// Converts an SVG file to a PNG file.
+/// Example: foo.svg becomes foo.svg.png
+pub fn svg2png(filename: String) -> Option<Box<dyn OutputImage>> {
+    println!("svg2png path: {}", &filename);
+    let mut opt = resvg::Options::default();
+    opt.usvg.path = Some(filename.clone().into());
+
+    let rtree = usvg::Tree::from_file(&filename, &opt.usvg).unwrap();
+    let backend = resvg::default_backend();
+    let img = backend.render_to_image(&rtree, &opt);
+    img
+}
+
+/// Extract extension from filename
+/// https://stackoverflow.com/questions/45291832/extracting-a-file-extension-from-a-given-path-in-rust-idiomatically
+pub fn get_extension(filename: &str) -> Option<&str> {
+    Path::new(filename).extension().and_then(OsStr::to_str)
 }
